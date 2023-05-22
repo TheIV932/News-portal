3ded8281a1788fea2649ba079c3e7910132deb38
TheIV932 <131748348+TheIV932@users.noreply.github.com>
2023-05-22 12:34:47 +0700

    Update userDaoImpl.java

--- Files/DAO/userDaoImpl.java
+++ Files/DAO/userDaoImpl.java
@@ -1,55 +1,44 @@
 package Files.Dao;
 
-public class articleDaoImpl {
-    private int id;
-    private String title;
-    private String content;
-    private userDaoImpl user;
+import java.util.Objects;
 
-    public articleDaoImpl() {}
+public class userDaoImpl {
+        private int id;
+        private String name;
 
-    public articleDaoImpl(int id, String title, String content, userDaoImpl user) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.user = userDaoImpl;
+        public userDaoImpl () {}
+
+        public userDaoImpl (int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() { return id; }
+        public void setId(int id) { this.id = id; }
+
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            userDaoImpl user = (userDaoImpl) o;
+            return id == user.id &&
+                    Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
+
+        @Override
+        public String toString() {
+            return "user{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-
-    public userDaoImpl getUser() { return user; }
-    public void setUser(userDaoImpl user) { this.user = user; }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        articleDaoImpl article = (articleDaoImpl) o;
-        return id == article.id &&
-                Objects.equals(title, article.title) &&
-                Objects.equals(content, article.content) &&
-                Objects.equals(user, article.user);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, content, user);
-    }
-
-    @Override
-    public String toString() {
-        return "article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                ", user=" + user +
-                '}';
-    }
-}
